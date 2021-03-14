373081b612904c102fdd81783ea0d55280b62bec
Sasha <sasha.tsatryan.01@gmail.com>
2021-03-15 03:03:25 +0400

    Small changes

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ require("./db/mongoose");
 const userRouter = require("./routers/user");
 const postRouter = require("./routers/post");
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.use(express.json());
 app.use(userRouter);
